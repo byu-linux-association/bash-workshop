bfe930ddfac01c90a3e3bfc0419769aaacc16cc7
Alec Davis <unlikelytitan@gmail.com>
2025-10-09 15:42:36 -0600

    added task files

---
+++ tasks/task1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This task is to iterate over everything in a directory and print out its name.
+# This script should be very similar to the `ls` command.
+#
+# Hint: use file globbing to get everything in a directory.
+# Also, don't use `ls` command anywhere in your code
---
+++ tasks/task3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# For this task, we have provided the Bible as a list of newline spaced words.
+# This file is bible.txt
+# You should write a shell pipeline that finds the 10 most common words in the Bible and their count.
+#
+# Hint: uniq, sort, and head are your friends
+# Also, always sort before using uniq
